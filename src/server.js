5d6308441728c3af5162bdd8cfd52655efb12020
Rafael Braga <rafael.braga@conexasaude.com.br>
2022-02-24 00:39:34 -0300

    Correção na subida

--- src/server.js
+++ src/server.js
@@ -1,5 +1,5 @@
-import app from './app';
+import App from './app';
 
-var listener = app.listen(process.env.PORT || 3333, function(){
+var listener = App.listen(process.env.PORT || 3333, function(){
     console.log('Listening on port ' + listener.address().port); //Listening on port 8888
 });
