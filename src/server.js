aff936e15759bf3c9277969ceb35b8261df83e32
Rafael Braga <rafael.braga@conexasaude.com.br>
2022-02-24 00:04:26 -0300

    Correção no deploy

--- src/server.js
+++ src/server.js
@@ -1,5 +1,5 @@
 import app from './app';
 
-var listener = app.listen(3333, function(){
+var listener = app.listen(process.env.PORT || 3333, function(){
     console.log('Listening on port ' + listener.address().port); //Listening on port 8888
 });
